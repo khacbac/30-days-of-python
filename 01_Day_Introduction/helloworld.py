2b89ae6823d2bff0f5cafc7a119cf40cfc6bfd2e
khacbac <hokhacbac@gmail.com>
2025-01-06 13:42:28 +0700

    day 1

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,21 @@
+# introduction to python
+# day 1 - 30 days of python
+
+print("Hello World")
+print(3+2) # addition
+print(3-2) # subtraction
+print(3*2) # multiplication
+print(3/2) # division
+print(3%2) # modulus
+print(3**2) # exponentiation
+print(3//2) # floor division
+
+# Checking data types
+print(type(10)) # integer
+print(type(3.14)) # float
+print(type(1+3j)) # complex
+print(type('Asabeneh')) # string
+print(type([1,2,3])) # list
+print(type({'name':'Asabeneh'})) # dictionary
+print(type({9.8,3.14,2.7})) # set
+print(type((9.8,3.14,2.7)))
